19b7483c5cc8e20eea504fc8c1941e56479a75f1
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-12 13:19:35 +0300

    commit tree 8568b5c720017773226af03602576fbdbcd6af69

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter(function(driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+
+function fuzzyMatch(drivers, letter){
+    return drivers.filter(function(driverName){
+        return driverName[0] === letter;
+    })
+}
+
+
+function matchName(drivers, name) {
+    return drivers.filter(function(details){
+        return details.name === name
+    })
+}
