43ba0c64bf6ab3caf3a86a62512c4770929748c7
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-12 13:37:35 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter(function(driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+
+function fuzzyMatch(drivers, letter){
+    return drivers.filter(function(driverName){
+        return driverName.toLowerCase().startsWith(letter.toLowerCase())
+    })
+}
+
+
+function matchName(drivers, name) {
+    return drivers.filter(function(details){
+        return details.name === name
+    })
+}
